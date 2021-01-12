fd2ba75f4013eba921abee488814c15b1cb32b68
alin <alinulms@gmail.com>
2021-01-12 16:13:37 +0100

    initial commit

---
+++ Controllers/TestLetterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.SampleNewsletter.Repositories;
+using Sitecore.Mvc.Presentation;
+
+namespace ServicesNewsletter.Controllers
+{
+    public class TestLetterController : Controller
+    {
+        private readonly ViewModelRepository _repository;
+
+        public TestLetterController():this(new ViewModelRepository())
+        {
+            
+        }
+        public TestLetterController(ViewModelRepository repository)
+        {
+            Assert.ArgumentNotNull((object)repository, nameof(repository));
+            this._repository = repository;
+        }
+
+        public ActionResult POQ()
+        {
+            return (ActionResult)this.View("~/sitecore modules/Web/Exm/layouts/TestLetter/POQ.cshtml", (object)this._repository.GetFixedSectionViewModel(RenderingContext.Current.Rendering));
+        }
+    }
+}
